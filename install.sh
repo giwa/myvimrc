cf0ea56eeeb5663ad8e3a28f1f11cb3d467caa33
giwa <ugw.gi.world@gmail.com>
2014-11-24 05:57:38 +0900

    replace python script to shell

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Get script directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cp "$DIR/vimrc ./.vimrc"
+
+# Create directory for vim bundle
+mkdir -p ~/.vim/bundle
+
+# Clone neobundle
+git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+
+# Clone solarized
+git clone https://github.com/altercation/vim-colors-solarized.git /tmp/solarized
+cp -rf /tmp/solarized/colors ./.vim/
+rm -rf /tmp/solarized
+
